0f4d93311a3919d34000f7d9edaca2fe55b61888
Nathalie Vialaneix <nathalie.vialaneix@inrae.fr>
2020-03-05 13:35:18 +0100

    automated package version in init message

--- R/init.R
+++ R/init.R
@@ -2,7 +2,7 @@
   packageStartupMessage("")
   packageStartupMessage("***********************************************************")
   packageStartupMessage("")
-  packageStartupMessage("      This is 'SOMbrero' package, v 1.2")
+  packageStartupMessage("      This is 'SOMbrero' package, v ", packageVersion("SOMbrero"))
   packageStartupMessage("")
   packageStartupMessage("Citation details with citation('SOMbrero')")
   packageStartupMessage("")
