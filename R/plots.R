e8e2d6ab0f551d34ac2cefedb0409a4e45877b91
Elise Maigné <elise.maigne@inrae.fr>
2020-03-24 08:22:07 +0100

    poly.dist OK. pie for add in ggplot2 (missing the size of the pies)

--- R/plots.R
+++ R/plots.R
@@ -66,126 +66,6 @@ myTitle <- function(args, what) {
   return(the.title)
 }
 
-# plotOneVariable <- function(ind, var.val, type, the.title, args) {
-#   args$ylab <- ""
-#   args$xlab <- ""
-#   if (type!="names" && type!="words" && !is.null(args$col) 
-#       && length(args$col)>1) {
-#     args$col <- args$col[ind]
-#     if ((type=="boxplot")&(!is.null(args$border)))
-#       args$border <- args$border[ind]
-#   }
-#   if (!is.null(the.title)) {
-#     args$main <- the.title
-#   } else args$main <- NULL
-#   if (all(is.na(var.val))) {
-#     plot(1,type="n",axes=F,xlab="",ylab="",main=args$main)
-#     if (type %in% c("lines","boxplot", "names", "words")) box()
-#   } else {
-#     if(type=="lines") {
-#       if (is.null(args$lwd)) args$lwd <- 2
-#       if (is.null(args$type)) args$type <- "l"
-#       if (is.null(args$col)) args$col <- "tomato"
-#       args$xaxt <- "n"
-#       args$yaxt <- "n"
-#       args$x <- var.val
-#       do.call("plot",args)
-#     } else if (type=="barplot") {
-#       args$height <- var.val
-#       args$axes <- FALSE
-#       if (is.null(args$col)) args$col <- "orange"
-#       if (is.null(args$names.arg)) args$names.arg <- ""
-#       if (is.null(args$border)) args$border <- FALSE
-#       do.call("barplot",args)
-#       abline(h=0, col=args$col)
-#     } else if (type=="boxplot") {
-#       args$x <- var.val
-#       args$axes <- FALSE
-#       if (is.null(args$names)) args$names <- FALSE
-#       if (is.null(args$col)) args$col <- "tan2"
-#       do.call("boxplot", args)
-#       box()
-#     } else if (type %in% c("names", "words")) {
-#       if (sum(var.val)>0) {
-#         if (is.null(args$min.freq)) args$min.freq <- 1
-#         args$words <- names(var.val)[var.val>0]
-#         args$freq <- sqrt(var.val[var.val>0])
-#         args$scale <- args$scale*max(var.val)
-#         if (is.null(args$ordered.colors)) args$ordered.colors <- TRUE
-#         if (is.null(args$rot.per)) args$rot.per <- 0
-#         do.call("wordcloud", args)
-#       } else plot(1,type="n",axes=F,xlab="",ylab="")
-#       box()
-#       title(the.title)
-#     }
-#   }
-# }
-
-# plotAllVariables <- function(what, type, values, clustering=NULL, print.title,
-#                              the.titles, is.scaled, the.grid, args) {
-#   par(paramGraph(the.grid, print.title, type))
-#   ordered.index <- orderIndexes(the.grid, type)
-#   if (!is.null(args$col) && length(args$col)>1 && 
-#         length(args$col)!=length(ordered.index)) {
-#     warning("unadequate number of colors; first color will be used for all\n", 
-#             call.=TRUE, immediate.=TRUE)
-#     args$col <- args$col[1]
-#   }
-#   if (print.title) {
-#     the.titles <- the.titles
-#   } else the.titles <- rep(NULL,length(ordered.index))
-#   if (type %in% c("names", "words")) {
-#     
-#     freq.words <- words2Freq(values, clustering, the.grid, type)
-#     if (is.null(args$colors)) {
-#       nb.breaks <- 6
-#       all.colors <- brewer.pal(9,"Purples")[5:9]
-#     } else if (length(args$colors)==1) {
-#       words.col <- matrix(args$colors, ncol=ncol(freq.words),
-#                           nrow=nrow(freq.words))
-#     } else {
-#       nb.breaks <- length(args$colors)+1
-#       all.colors <- args$colors
-#     }
-#     if (is.null(args$colors)|length(args$colors)>1) {
-#       the.breaks <- seq(min(freq.words[freq.words>0])-0.1,
-#                         max(freq.words)+0.1,length=nb.breaks)
-#       words.cut <- apply(freq.words,2,cut,breaks=the.breaks,labels=FALSE)
-#       words.col <- apply(words.cut, 2, function(wc) all.colors[wc])
-#     }
-#     if (is.null(args$scale)) {
-#       args$scale <- c(2,0.5)/max(freq.words)
-#     } else args$scale <- args$scale/max(freq.words)
-#     sapply(ordered.index, function(ind) {
-#       cur.args <- args
-#       if (sum(freq.words[ind,])>0) {
-#         cur.args$colors <- words.col[ind,freq.words[ind,]>0]
-#       }
-#       plotOneVariable(ind, freq.words[ind,], type, the.titles[ind], cur.args)
-#     })
-#   } else {
-#     is.scaled.values <- scale(values,is.scaled, is.scaled)
-#     args$ylim <- range(is.scaled.values)
-#     if (what=="prototypes"|type=="boxplot") {
-#       mean.val <- is.scaled.values
-#     } else mean.val <- averageByCluster(is.scaled.values, clustering, the.grid)
-#     if (type=="boxplot") {
-#       sapply(ordered.index, function(ind) {
-#         plotOneVariable(ind,matrix(as.matrix(mean.val)[which(clustering==ind),],
-#                                    ncol=ncol(as.matrix(values))), type, 
-#                         the.titles[ind], args)
-#       })
-#     } else {
-#       sapply(ordered.index, function(ind) {
-#         plotOneVariable(ind,mean.val[ind,], type, the.titles[ind], args)
-#       })
-#     }
-#   }
-#   title(main=myTitle(args, what), outer=TRUE)
-#   par(mfrow=c(1,1), oma=c(0,0,0,0), mar=c(5, 4, 4, 2)+0.1)
-# }
-
-
 
 plot3d <- function(x, the.grid, type, variable, args) {
   if(is.null(args$topo)==F) args <- args[which(names(tmpargs)== "topo")]
@@ -206,50 +86,53 @@ plot3d <- function(x, the.grid, type, variable, args) {
   do.call("persp", args)
 }
 
-plotOnePolygon <- function(ind, values, the.grid, col) {
-  ## FIX IT think about a more general way to implement it
+
+coords_polydist <- function(ind, values, the.grid) {
   cur.coords <- the.grid$coord[ind,]
-  if (the.grid$topo=="square") {
-    neighbors <- match(paste(rep((cur.coords[1]-1):(cur.coords[1]+1),c(3,3,3)),
-                             rep((cur.coords[2]-1):(cur.coords[2]+1),3)), 
-                       paste(the.grid$coord[,1], the.grid$coord[,2]))
-    neighbors <- neighbors[-5]
-    poly.coord <- matrix(nrow=8, ncol=2)
-    poly.coord[is.na(neighbors),] <- tcrossprod(rep(1,sum(is.na(neighbors))),
-                                                cur.coords)
-    active.indexes <- setdiff(1:8,which(is.na(neighbors)))
-    poly.coord[active.indexes,] <- t(sapply(seq_along(active.indexes),
-                                            function(ind2) {
-      n.ind <- neighbors[active.indexes[ind2]]
-      n.coords <- the.grid$coord[n.ind,]
-      cur.coords + (n.coords-cur.coords)*values[ind2]
-    }))
-    poly.coord <- poly.coord[c(1,4,6,7,8,5,3,2),]
-    polygon(poly.coord, col=col)
-  } else {
-    stop("Sorry: 'polygon' is still to be implemented for this dist.type or/and
-          this topology.", call.=TRUE)
+  cur.values <- values[[ind]]
+  neigh <- as.numeric(as.character(names(values[[ind]])))
+  
+  maxnbneigh <- 8
+  if(the.grid$topo=="hexagonal"){
+    maxnbneigh <- 6
   }
+  
+  poly.coord <- matrix(nrow=length(neigh), ncol=2)
+  for(i in 1:length(neigh)){
+      n.coords <- the.grid$coord[neigh[i],]
+      poly.coord[i,] <- cur.coords + (n.coords-cur.coords)*cur.values[i]
+  }
+  # For borders
+  if(length(neigh)<maxnbneigh){
+    poly.coord <- rbind(poly.coord, cur.coords)
+  }
+  
+  poly.coord <- data.frame(poly.coord)
+  colnames(poly.coord) <- c("x", "y")
+  
+  # Sort poly.coord in clockwise order. 
+  # Code from https://github.com/skgrange/gissr
+  x_centre <- mean(poly.coord$x)
+  y_centre <- mean(poly.coord$y)
+  # Calculate deltas
+  poly.coord$x_delta <- poly.coord$x - x_centre
+  poly.coord$y_delta <- poly.coord$y - y_centre
+  
+  # Resolve angle, in radians
+  poly.coord$angle <- atan2(poly.coord$y_delta, poly.coord$x_delta)
+  poly.coord <- poly.coord[order(poly.coord$angle, decreasing = TRUE), ]
+
+  # Drop intermediate variables
+  poly.coord[, c("x_delta", "y_delta", "angle")] <- NULL
+
+  poly.coord$id <- ind
+  rownames(poly.coord) <- paste0(ind, ".", 1:nrow(poly.coord))
+  
+  print(poly.coord)
+  return(poly.coord)
 }
 
 
-plotPolygon <- function(values, clustering, the.grid, my.palette, args) {
-  plot.args <- c(list(x=the.grid),args)
-  do.call("plot.myGrid",plot.args)
-  if (is.null(my.palette)) {
-    nb.breaks <- min(c(floor(1 + (3.3*log(prod(the.grid$dim), base=10))),9))
-  } else nb.breaks <- length(my.palette)
-  the.breaks <- seq(0,max(table(clustering)),length=nb.breaks+1)
-  if (is.null(my.palette)) {
-    my.colors <- heat.colors(nb.breaks)[nb.breaks:1]
-  } else my.colors <- my.palette
-  freq.clust <- sapply(1:prod(the.grid$dim), function(ind) sum(clustering==ind))
-  vect.color <- my.colors[cut(freq.clust, the.breaks, labels=FALSE)]
-  vect.color[is.na(vect.color)] <- "white"
-  invisible(sapply(1:prod(the.grid$dim), function(ind) {
-    plotOnePolygon(ind, values[[ind]], the.grid, vect.color[ind])
-  }))
-}
 
 ### SOM algorithm graphics
 plotPrototypes <- function(sommap, type, variable, my.palette, print.title,
@@ -325,18 +208,8 @@ plotPrototypes <- function(sommap, type, variable, my.palette, print.title,
         stop("Impossible to plot 'poly.dist'!", call.=TRUE)
       } else values <- lapply(values,sqrt)
     }
-    maxi <- max(unlist(values))
-    values <- lapply(values, function(x) 0.429*((maxi-x)/maxi+0.05))
-    # ggplotFacet("prototypes", "poly", values, sommap$clustering, 
-    #             print.title, the.titles, is.scaled, 
-    #             sommap$parameters$the.grid, args)
-    plotPolygon(values, sommap$clustering, sommap$parameters$the.grid,
-                my.palette, args)
-    if (print.title) {
-      text(x=sommap$parameters$the.grid$coord[,1]-0.1,
-           y=sommap$parameters$the.grid$coord[,2]+0.1,
-           labels=the.titles, cex=0.7)
-    }
+    ggplotPolydist(values, sommap$clustering, print.title,
+                               the.titles, sommap$parameters$the.grid)
   } else if (type=="umatrix" || type=="smooth.dist") {
     args$topo <- sommap$parameters$the.grid$topo
     values <- protoDist(sommap, "neighbors")
@@ -613,15 +486,6 @@ plotAdd <- function(sommap, type, variable, proportional, my.palette,
     }
     ggplotFacet("add", type, variable, sommap$clustering, print.title, the.titles, 
                 is.scaled, sommap$parameters$the.grid, args)
-    # plotAllVariables("add", type, variable, sommap$clustering, print.title,
-    #                  the.titles, is.scaled, sommap$parameters$the.grid, args)
-  # } else if (type=="names") {
-  #   if (ncol(variable) != 1) {
-  #     stop("for type='names', argument 'variable' must be a vector", call.=TRUE)
-  #   }
-  #   plotAllVariables("add", type, as.character(variable), sommap$clustering, 
-  #                    print.title, the.titles, is.scaled,
-  #                    sommap$parameters$the.grid, args)
   } else if (type=="graph") {
     # controls
     if (!is_igraph(variable)){
@@ -756,7 +620,7 @@ plot.somRes <- function(x, what=c("obs", "prototypes", "energy", "add"),
   switch(what,
          "prototypes"=plotPrototypes(x, type, variable, my.palette, print.title,
                                      the.titles, is.scaled, view, args),
-         "energy"=plotEnergy(x, args),
+         "energy"=ggplotEnergy(x),
          "add"=plotAdd(x, type, if (type!="graph") as.matrix(variable) else 
            variable, proportional, my.palette, print.title, the.titles, 
                        is.scaled, s.radius, pie.graph, pie.variable, args),
