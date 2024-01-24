abe0cebc1e3479193a18596138f3ee5e5025b465
Nathalie Vialaneix <nathalie.vialaneix@inrae.fr>
2024-01-24 19:36:16 +0100

    moved SOMbrero repository

--- R/distance_helpers.R
+++
@@ -1,9 +0,0 @@
-# Helpers for distance computation ############################################
-
-## distance between rows of two matrices
-
-matrix.dist <- function(mat1, mat2) {
-  outer(1:nrow(mat1), 1:nrow(mat2), Vectorize(function(r1, r2) {
-    dist(rbind(mat1[r1, ], mat2[r2, ]))
-  }))
-}
--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library("testthat")
-library("SOMbrero")
-
-test_check("SOMbrero")
